d18930f4acabf69d8dd718ab5a2686a9bfd7f048
Ian Carper <Itcarper@ymail.com>
2022-05-25 10:14:40 -0500

    React project

---
+++ React_Portfolio/src/index.js
@@ -0,0 +1,25 @@
+ReactDOM.render(<h1><img src="Pictures/HeadShot.jpg" alt="Picture of Ian Carper"></h1>,document.getElementById("picture"))
+ReactDOM.render(<h1>Portfolio.</h1>, document.getElementById("title"))
+ReactDOM.render(<h1>This is all about me.</h1>, document.getElementById("h1"))
+ReactDOM.render(<p> I have been programing for 5 years during my physics undergraduates.Most of my experience comes from programming in python but java is a close second. Regardless of the language I would always have fun</p>, document.getElementById("aboutMe"))
+ReactDOM.render(<h1>This is my education.</h1>, document.getElementById("h2"))
+ReactDOM.render(<p>B.S. of Physics with a concentration of computer science at University of louisiana at lafayette</p>,document.getElementById("education"))
+ReactDOM.render(<h1>This is my past experiences</h1>, document.getElementById("h3"))
+ReactDOM.render(<p> Food service - 6 yrs</p>, document.getElementById("experience"))
+ReactDOM.render(<h1>These are my skills</h1>, document.getElementById("h4"))
+ReactDOM.render(<ul> <li>Microservices</li> <li>React </li><li>Python </li><li>Latex</li><li>Core Java</li><li>Hibernate</li><li>Spring </li><li>Spring Boot</li><li>MySQL</li><li>mysql workbench</li><li>Postman</li><li>APIs</li><li>GitHub</li><li>GitKraken</li><li>HTML</li><li>CSS</li><li>gitlab</li><li>JSX</li><li>Javascript</li><li>AWS</li></ul>, document.getElementById("skillList"))
+ReactDOM.render(<h1>Contact and websites</h1>, document.getElementById("h5"))
+ReactDOM.render(<p><a href="https://github.com/Smasherlasher/GenSpark-Projects">Github</a><br></br>Email: Itcarper@ymail.com</p>, document.getElementById("contact"))
+
+
+
+
+
+
+
+
+
+
+
+
+
